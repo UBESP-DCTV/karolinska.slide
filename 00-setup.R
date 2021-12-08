942b3d2b12a610cb109f12c5e0d5131d21181002
Corrado <corrado.lanera@gmail.com>
2021-12-08 23:39:03 +0100

    First draft

--- 00-setup.R
+++ 00-setup.R
@@ -1,15 +1,20 @@
-renv::install(c(
-  # CRAN
-  "xaringan", "tidyverse", "usethis", "devtools", "blogdown", "knitr",
-  "markdown", "spelling", "here", "distill", "fontawesome",
 
-  # GitHub
+# Packages --------------------------------------------------------
+
+renv::install(c(
+  ## CRAN
+  "xaringan", "tidyverse", "usethis", "devtools", "blogdown", "knitr",
+  "markdown", "spelling", "here", "distill", "fontawesome", "miniUI",
+
+  ## GitHub
   "gadenbuie/xaringanthemer",
   "gadenbuie/metathis",
   "gadenbuie/xaringanExtra",
   "hadley/emo"
 ))
 
+
+# Project setup ---------------------------------------------------
 
 usethis::use_description(check_name = FALSE)
 usethis::use_readme_rmd()
@@ -19,18 +24,30 @@ usethis::use_code_of_conduct("corrado.lanera@ubep.unipd.it")
 usethis::git_vaccinate()
 
 
+# Requirements ----------------------------------------------------
 
-renv::status()
-renv::snapshot()
-
-# mandatory packages to compile the slides
+## mandatory packages to compile the slides
 list(
-  "xaringan"
+  "xaringan", "fontawesome", "knitr"
 ) |>
   purrr::walk(usethis::use_package)
 
-# optional packages used within the project
 list(
-  "usethis", "renv", "purrr"
+  "xaringanExtra", "xaringanthemer"
+) |>
+  purrr::walk(usethis::use_dev_package)
+
+
+## optional packages used within the project
+list(
+  "usethis", "renv", "purrr", "devtools", "miniUI"
 ) |>
   purrr::walk(usethis::use_package, type = "Suggests")
+
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+
+
+# Isolation -------------------------------------------------------
+
+renv::status()
+renv::snapshot()
